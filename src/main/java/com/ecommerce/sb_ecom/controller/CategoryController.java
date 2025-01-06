db640ee7534786e85118faedb7e0805f3f820fde
Jagrit29 <jagritbhupal29@gmail.com>
2025-01-06 11:16:09 +0530

    Section6: CategoryService Layer with interface

--- src/main/java/com/ecommerce/sb_ecom/controller/CategoryController.java
+++ src/main/java/com/ecommerce/sb_ecom/controller/CategoryController.java
@@ -1,6 +1,8 @@
 package com.ecommerce.sb_ecom.controller;
 
 import com.ecommerce.sb_ecom.model.Category;
+import com.ecommerce.sb_ecom.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,17 +13,21 @@ import java.util.ArrayList;
 
 @RestController
 public class CategoryController {
-    private List<Category> categories = new ArrayList<>();
+    private CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
 
     @GetMapping("/api/public/categories")
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryService.getAllCategories();
     }
 
     @PostMapping("/api/admin/categories")
     public String createCategory(@RequestBody Category category) {
-        categories.add(category);
-
+        categoryService.createCategory(category);
         return "Category added successfully";
     }
 
